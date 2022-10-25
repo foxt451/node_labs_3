920ab1a0789d4fb8da35d638f19f24473f056488
foxt451 <svpetrenko123@gmail.com>
2022-10-26 01:49:55 +0300

    add functions folder

--- hosting/src/index.ts
+++ hosting/src/index.ts
@@ -7,6 +7,7 @@ const requestListener: RequestListener = (req, res) => {
 
 const server = http.createServer(requestListener);
 const PORT = process.env.PORT ?? 8080;
-const a = 3;
+console.log(a);
+
 server.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
