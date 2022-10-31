f708318e714aa828979355715d84ee8d8b4d38b1
foxt451 <svpetrenko123@gmail.com>
2022-10-31 11:27:35 +0200

    test workflow execution

--- hosting/src/index.ts
+++ hosting/src/index.ts
@@ -2,7 +2,7 @@ import http, { RequestListener } from "http";
 
 const requestListener: RequestListener = (req, res) => {
   res.writeHead(200);
-  res.end("Hello, Mom!");
+  res.end("Hello, Mom!!!");
 };
 
 const server = http.createServer(requestListener);
